db21f8277e90f4d2c4c9281e62ac3f2ca7a612f2
Brett Zamir <brettz9@yahoo.com>
2019-05-19 17:44:49 +0800

    - More precise logging

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const OpenWith = require('macos-defaults/OpenWith');
 (async () => {
 try {
   const openWith = new OpenWith();
-  const result = await openWith.getAsync(join(__dirname, 'index.js'));
-  console.log('r', result);
+  const {
+    bundleidentifier, path
+  } = await openWith.getAsync(join(__dirname, 'index.js'));
+  console.log('bundleidentifier', bundleidentifier);
+  console.log('path', path);
 } catch (err) {
   console.log('er', err);
 }
