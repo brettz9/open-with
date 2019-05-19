dcff391f09673e6ef2fe46884589cbdaf490d0e3
Brett Zamir <brettz9@yahoo.com>
2019-05-19 18:56:02 +0800

    - lsregister testing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 const {join, extname} = require('path');
 const mdls = require('mdls');
+const lsregister = require('lsregister');
 const OpenWith = require('macos-defaults/OpenWith');
 const {MacOSDefaults} = require('macos-defaults');
 
@@ -20,11 +21,36 @@ try {
 }
 
 try {
-  const data = await mdls(filePath, '-name kMDItemContentTypeTree');
-  console.log('Data', data);
+  const {
+    ItemContentTypeTree
+  } = await mdls(filePath, '-name kMDItemContentTypeTree');
+  console.log('Data', ItemContentTypeTree);
 } catch (err) {
   console.log('Error', err);
 }
+
+// Todo: Use mdls data above with this lsregister below:
+// eslint-disable-next-line max-len
+// /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | grep -n7 'public'
+try {
+  const result = await lsregister.dump();
+  console.log('result', result.length);
+  console.log('r', result.filter(({
+    bindings, contentType, extension, uti, serviceId,
+    bundleClass, containerMountState, extPointID, uRLScheme,
+    claimId, volumeId, ...others
+  }) => {
+    return !bindings && !contentType && !extension &&
+      !uti && !serviceId && !bundleClass && !containerMountState &&
+      !extPointID && !uRLScheme && !claimId && !volumeId &&
+      !Object.keys(others).some((item) => item.startsWith('pluginIdentif'));
+    // return bindings && (bindings.includes('.js') ||
+    //    bindings.includes('javascript'));
+  }));
+} catch (err) {
+  console.log('Error', err);
+}
+return;
 
 try {
   const macOSDefaults = new MacOSDefaults();
@@ -34,7 +60,7 @@ try {
   });
   console.log('result', lsHandlers);
 
-  // Todo: Also allow a filter per mdls content-type results
+  // Todo: Also allow a filter from launchservices per mdls content-type results
 
   const ext = extname(filePath).slice(1);
   const matchingExtensions = lsHandlers.filter(({
