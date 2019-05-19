d89485fb222101ac4a5c244dd30672f6da1b30fb
Brett Zamir <brettz9@yahoo.com>
2019-05-19 18:06:18 +0800

    - Get by extension name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 'use strict';
-const {join} = require('path');
+const {join, extname} = require('path');
 const mdls = require('mdls');
 const OpenWith = require('macos-defaults/OpenWith');
 const {MacOSDefaults} = require('macos-defaults');
@@ -28,11 +28,23 @@ try {
 
 try {
   const macOSDefaults = new MacOSDefaults();
-  const result = await macOSDefaults.read({
+  const lsHandlers = await macOSDefaults.read({
     domain: 'com.apple.LaunchServices/com.apple.launchservices.secure',
     key: 'LSHandlers'
   });
-  console.log('result', result);
+  console.log('result', lsHandlers);
+
+  // Todo: Also allow a filter per mdls content-type results
+
+  const ext = extname(filePath).slice(1);
+  const matchingExtensions = lsHandlers.filter(({
+    LSHandlerContentTagClass,
+    LSHandlerContentTag
+  }) => {
+    return LSHandlerContentTagClass === 'public.filename-extension' &&
+      LSHandlerContentTag === ext;
+  });
+  console.log('matchingExtensions', matchingExtensions);
 } catch (err) {
   console.log('Err', err);
 }
